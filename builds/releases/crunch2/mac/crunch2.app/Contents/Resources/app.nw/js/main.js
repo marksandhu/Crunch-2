b58e310c9fa34f50dc52b4606710902042b01764
Mark S <mark.sandhu@gmail.com>
2014-01-26 22:21:03 -0800

    Testing grunt-node-webkit-builder plugin to create platform dependant builds

---
+++ builds/releases/crunch2/mac/crunch2.app/Contents/Resources/app.nw/js/main.js
@@ -0,0 +1,36 @@
+// Loads the application
+(function(global) { 
+	var curlCfg = {
+		baseUrl: 'js',
+		curl: 'lib/curl/curl.min',
+		paths: {
+			//jquery: 'lib/jquery',  // jQuery, you're dead unless we become desperate
+			css: '../css',
+			text: 'lib/curl/plugin/text',
+			'_fetchText': 'lib/curl/plugin/_fetchText'
+		},
+		defineName: 'def'
+	};
+
+	curl(curlCfg, ['lib/ractive.min', 'js!lib/ace/ace', 'link!css/styles.css']).then(function ($) {
+		
+		// init the app
+		curl(['crunch/core', 'crunch/prefs', 'crunch/session', 'domReady!']).then(function() {
+			init();	
+		});
+		
+	});
+
+
+	function init() {
+		document.body.className = 'loaded';
+
+		// $('body').addClass('loaded');
+		// $('.crunch').click(function() {
+		// 	$(this).closest('.tab-out').toggleClass('flipped');
+		// });
+		// $('.tab-out').click(function() {
+		// 	$(this).addClass('active').siblings().removeClass('active flipped');
+		// });
+	}
+})(this);
